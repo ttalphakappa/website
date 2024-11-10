cd57db316357a5f91c8ab3818bc5f4796ff3c4a5
Lily Pad Plunge <ttalphakappa@gmail.com>
2024-11-09 21:35:52 -0800

    updating website

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,11 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Lily Pad Plunge",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Lily Pad Plunge",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
@@ -19,7 +19,7 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
+  title: "Lily Pad Plunge",
   logo_name: "AshutoshHathidara",
   nickname: "layman_brother",
   subTitle:
